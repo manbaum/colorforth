7a9eee7e5cba015f6aaac51ac8ef423269a94959
jcomeau_ictx <jcomeau_ictx@users.sourceforge.net>
2016-01-12 01:38:17 +0000

    getting started with testfloppy.asm

--- colorforth/jc2007/testfloppy.asm
+++ colorforth/jc2007/testfloppy.asm
@@ -1,2 +1,12 @@
 ;# program to load onto a USB key to see what a minimally passing "floppy"
 ;# is to my BIOS.
+.intel_syntax noprefix ;# floppy boot segment
+.code16
+.org 0
+boot:
+	jmp	start
+	nop
+	.org 0x3 ;# just insurance
+	.asciz "cmcf 1.0"
+start:
+	nop
